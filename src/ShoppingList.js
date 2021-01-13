c1463edab48b585877eea3f582e9c09a7d55995e
jthickens <47118049+jthickens@users.noreply.github.com>
2021-01-13 17:23:52 -0500

    refactor: remove correction comments

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -6,7 +6,6 @@ export class ShoppingList extends React.Component {
   render() {
     return (
       <div className="shopping-list" role="articel" tabindex="3">
-      {/* <div className="shopping-list" role="article" tabindex="-1"> */}
         <h1>Shopping List for {this.props.name}</h1>
         <ul>
           <li>Instagram</li>
@@ -15,10 +14,8 @@ export class ShoppingList extends React.Component {
         </ul>
         <form>
           <input aria-labeledby="terms" type="checked" tabindex="1" autoFocus />
-          {/* <input aria-labelledby="terms" type="checked"   /> */}
           <div id="terms">Yes, I accept the terms and conditions.</div>
           <div role="butten" aria-disable="true">
-          {/* <div role="button" aria-disabled="true"> */}
             Purchase
           </div>
         </form>
