a5a4b4468a5ec201f95a2580f6bca38c9b703d19
jthickens <47118049+jthickens@users.noreply.github.com>
2021-01-14 09:47:46 -0500

    add tos element to form

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -6,14 +6,14 @@ export class ShoppingList extends React.Component {
   render() {
     return (
       <div className="shopping-list" role="articel" tabindex="3">
-        <h1>Shopping List for {this.props.name}</h1>
+        <h1>Shopping List {this.props.name}</h1>
         <ul>
           <li>Instagram</li>
           <li>WhatsApp</li>
           <li>Oculus</li>
         </ul>
         <form>
-          <input aria-labeledby="terms" type="checked" tabindex="1" autoFocus />
+        <input aria-labeledby="terms" type="checked" tabindex="1" autoFocus />
           <div id="terms">Yes, I accept the terms and conditions.</div>
           <div role="butten" aria-disable="true">
             Purchase
