cbcf9cdedcfde65acd4fe0e5047d7556c583831b
Jonathan Thickens <47118049+jthickens@users.noreply.github.com>
2021-01-14 10:02:03 -0500

    Update src/ShoppingList.js
    Co-authored-by: axe-linter[bot] <63360224+axe-linter[bot]@users.noreply.github.com>

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -13,7 +13,7 @@ export class ShoppingList extends React.Component {
           <li>Oculus</li>
         </ul>
         <form>
-        <input aria-labeledby="terms" type="checked" tabindex="1" autoFocus />
+        <input aria-labelledby="terms" type="checked"   />
           <div id="terms">Yes, I accept the terms and conditions.</div>
           <div role="butten" aria-disable="true">
             Purchase
